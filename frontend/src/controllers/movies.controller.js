e76de9544635db1a4b817f0456a7c021f5f800bb
BordeaP <paul.bordea@verivox.com>
2019-08-03 10:13:09 +0300

    Hookup movie page to db server

--- frontend/src/controllers/movies.controller.js
+++ frontend/src/controllers/movies.controller.js
@@ -5,45 +5,20 @@
 
     })
     app.controller('movieUser',
-    function Control($scope){
-        $scope.model={
-            usermovies:[{
-                id: 2,
-            
-                title: "Ben",
-                date: "7/10/2019",
-                time: "12:00",
-                room: "room 2",
-                actors: "John Snow, Emilia Clarke",
-                year: "2018",
-                description: "A fascinating story about a little boy who is going to cnjhnbhjfdnjjfjfjjdjdjghjdhhgjd"
-            },
-            {
-                id: 1,
-                title: "Ben",
-                date: "7/10/2019",
-                time: "12:00",
-                room: "room 255",
-                actors: "John Snow, Emilia Clarke",
-                year: "2018",
-                description: "A fascinating story about a little boy who is going to cnjhnbhjfdnjjfjfjjdjdjghjdhhgjd"
-    
-            },
-            {
-                id: 3,
-                title: "Ben",
-                date: "7/10/2019",
-                time: "12:00",
-                room: "room 2",
-                actors: "Jnow, Emilia Clarke",
-                year: "2018",
-                description: "A fascinating story about a little boy who is going to cnjhnbhjfdnjjfjfjjdjdjghjdhhgjd"
-    
+        function Control($scope, $http) {
+
+            $scope.movies = {};
+
+            $http.get("http://localhost:3000/movies")
+                .then((response) => {
+                    $scope.movies = response.data;
+                })
+                .catch((error) => {
+                    $log.log("Error fetching movies: " + JSON.stringify(error));
+                });
+
+            $scope.getTemplate = function(movie) {
+                return 'display';
             }
-        ]
-        }
-        $scope.getTemplate=function(movie){
-            return 'display';
-        }
-    })
+        })
 }());
