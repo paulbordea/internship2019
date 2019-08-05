d9eba0fe64b762ab63c3666e5d5469a5ba6e0b20
ramonaN <ramona.nechita99@gmail.com>
2019-08-05 15:52:07 +0300

    refactoring

--- frontend/src/controllers/home.controller.js
+++ frontend/src/controllers/home.controller.js
@@ -2,7 +2,15 @@
     'use strict';
 
     app.controller('homeCtrl', function($scope) {
-
+        $scope.menuItems = {
+            options:
+            [{menu:'Home',link:'#',class:"nav-link",datatoggle:""},
+            {menu: 'Movies',link:"/index.html#!/movies",class:"nav-link",datatoggle:""},
+            {menu: 'Login/Register',link:"#myModal",class:"nav-link trigger-btn", datatoggle:"modal"}]};
+        $scope.activeMenu = $scope.menuItems.options[0];
+        $scope.setActive = function(menuItem) {
+            $scope.activeMenu = menuItem
+         }
     })
 
 }());
