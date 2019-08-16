d93a20f92560be5725459c02bbb0d181f9bca79c
ramonaN <ramona.nechita99@gmail.com>
2019-08-16 15:04:40 +0300

    booking controller modified/deleted service

--- frontend/src/controllers/bookingController.js
+++ frontend/src/controllers/bookingController.js
@@ -1,81 +1,58 @@
-app.controller('bookingController', ['$scope', '$window', '$http', '$routeParams', '$log', 'seatsManager',
-    function($scope, $window, $http, $routeParams, $log, seatsManager) {
+app.controller('bookingController', ['$scope', '$http', '$routeParams', '$log',
+    function($scope, $http, $routeParams, $log) {
+        var config = {
+            headers : {
+                'Content-Type': 'application/json'
+            }
+        }
+    
         var init = function() {
 
             $scope.movieId = $routeParams.movieId;
-            $scope.standardSeats = seatsManager.getSeats('Standard');
-            $scope.seats = seatsManager;
+           
 
             $http.get("http://localhost:3000/seats1")
                 .then((response) => {
                     $scope.seats1 = response.data;
-                    $log.log('Seats: ' + JSON.stringify($scope.seats1));
+                    
+                     $scope.select=function(seat){
+
+                         if(seat.check===true){
+                             seat.check=false;
+                         }
+                         else{
+                             seat.check=true;
+                         }
+                     }
+                     $scope.storeSeat=function(){
+                        $scope.selection=[];
+                        for (var row = 0; row < $scope.seats1.length; row++) {
+                            for (var col = 0; col <$scope.seats1[row].length; col++) {
+                                if ($scope.seats1[row][col].check) {
+                                    $scope.selection.push($scope.seats1[row][col].seat_no);
+                                    $scope.seats1[row][col].free = false;
+                                    $scope.seats1[row][col].check = false;
+                                }
+ 
+                                
+                            }
+                        }
+                        $http.put(`http://localhost:3000/seats1`,$scope.seats1,config)
+                                .then((response)=>{
+                                    console.log(response.data);
+                                })
+                             .catch((error) => {
+                                    $log.log("Error fetching seats: " + JSON.stringify(error));
+                                });
+                        
+                        
+                     }
+                    
                 })
                 .catch((error) => {
                     $log.log("Error fetching seats: " + JSON.stringify(error));
                 });
-
-            $scope.quantities = [{
-                id: 0,
-                val: 0
-            }, {
-                id: 1,
-                val: 1
-            }, {
-                id: 2,
-                val: 2
-            }, {
-                id: 3,
-                val: 3
-            }, {
-                id: 4,
-                val: 4
-            }, ];
-
-            $scope.seatQuality = 'Standard';
-            $scope.selectedCount = $scope.quantities[1];
-            seatsManager.setAvailCount($scope.selectedCount);
         }
-
-
-        function storeSeatInSession(rang, row, seatIndex) {
-            if (angular.isUndefined(currentSelectionSession
-                    .checkedSeats[rang])) {
-                currentSelectionSession
-                    .checkedSeats[rang] = [];
-            }
-            var seat = angular.copy(row[seatIndex]);
-            delete seat['$$hashKey'];
-            delete seat['check'];
-            delete seat['booked'];        
-            currentSelectionSession.checkedSeats[rang].push(seat);
-             console.log(currentSelectionSession);
-        }
-        $scope.storeSeat = function() {
-            if ($scope.seats.availCount.val != 0) {
-                $window.alert("You haven't selected " +
-                    $scope.seats.availCount.val + " seats");
-                return;
-            }
-            var sessionInfo = seatsManager.bookCheckedSeats();
-            seatsManager.setAvailCount($scope.selectedCount);
-
-            console.log(sessionInfo.checkedSeats);
-
-            $scope.alertMsg = [];
-            angular.forEach(sessionInfo.checkedSeats, function(v, k) {
-                for (var i = 0; i < v.length; i++) {
-                    $scope.alertMsg.push(v[i].val + v[i].letter);
-                }
-                console.log($scope.alertMsg)
-            });
-
-            /*  $window.alert('Thank you for Booking ' + sessionInfo.count + ' seats. ' + 
-                     'Your seats are: ' + $scope.alertMsg.join(', ')); */
-
-        };
-
-
-        init();
-    }
+    init();
+     }
 ]);
